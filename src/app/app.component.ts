4539703a042c2ef207a77e2fe8add0f7743602ca
AnkitMahana <ankit.engineer1234@gmail.com>
2017-10-24 22:28:55 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Process Monitor';
+  data: any[] = [{"init_time":1508806800000,"data_download_time":100,"data_cleaning_time":150,"data_processing_time":100,"data_size":3,"row_count":5000,"status":"success"},
+  {"init_time":1508814000000,"data_download_time":120,"data_cleaning_time":200,"data_processing_time":150,"data_size":3.5,"row_count":9700,"status":"success"},
+  {"init_time":1508821200000,"data_download_time":0,"data_cleaning_time":0,"data_processing_time":0,"data_size":0,"row_count":9700,"status":"failed"},
+  {"init_time":1508832000000,"data_download_time":190,"data_cleaning_time":220,"data_processing_time":250,"data_size":4.5,"row_count":14080,"status":"success"},
+  {"init_time":1508842800000,"data_download_time":110,"data_cleaning_time":150,"data_processing_time":100,"data_size":3,"row_count":19679,"status":"success"},
+  {"init_time":1508850000000,"data_download_time":180,"data_cleaning_time":180,"data_processing_time":120,"data_size":4.1,"row_count":23679,"status":"success"},
+  {"init_time":1508857200000,"data_download_time":0,"data_cleaning_time":0,"data_processing_time":0,"data_size":0,"row_count":23679,"status":"failed"},
+  {"init_time":1508868000000,"data_download_time":200,"data_cleaning_time":150,"data_processing_time":190,"data_size":3.7,"row_count":27679,"status":"success"},
+  {"init_time":1508875200000,"data_download_time":150,"data_cleaning_time":180,"data_processing_time":120,"data_size":3.5,"row_count":32789,"status":"success"},
+  {"init_time":1508882400000,"data_download_time":110,"data_cleaning_time":150,"data_processing_time":115,"data_size":3,"row_count":37898,"status":"success"}];
+
+}
---
+++ src/app/data.service.ts
@@ -0,0 +1,8 @@
+import { injectable } from '@angular/core';
+@injectable()
+export class Dataservice extends AnotherClass {
+	
+	constructor(argument) {
+		// code...
+	}
+}
